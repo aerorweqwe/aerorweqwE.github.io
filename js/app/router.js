6650d2911570c210f5b6849f9eebd422ffa76ece
artem <artemgoogll@gmail.com>
2025-02-10 11:03:16 +0100

    sx

--- js/app/router.js
+++ js/app/router.js
@@ -2,6 +2,7 @@ import {login} from './pages/login.js';
 import {campaigns} from './pages/campaigns.js'; 
 import {campaign} from './pages/campaign.js'; 
 import {users} from './pages/users.js'; 
+import {user} from './pages/user.js'; 
 
 
 export const router = VueRouter.createRouter({
@@ -10,7 +11,8 @@ export const router = VueRouter.createRouter({
         { path: '/', name:'Sign in', component: login },
         { path: '/campaigns', name:'Campaigns', component: campaigns },
         { path: '/campaign/:id', name: 'Campaign', component: campaign },
-        { path: '/users', name:'Users', component: users }
+        { path: '/users', name:'Users', component: users },
+        { path: '/user/:id', name: 'User', component: user },
     ]
 });
 
